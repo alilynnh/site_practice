8e87e8bc62bf24d54180817c4c7f20b946e852aa
alilynnh <90108950+alilynnh@users.noreply.github.com>
2022-02-07 14:38:58 -0600

    landing done

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+VANTA.FOG({
+    el: "#landing",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    highlightColor: 0x7c6966,
+    midtoneColor: 0x443835,
+    lowlightColor: 0x301c1a,
+    baseColor: 0x443835,
+    speed: 2.00,
+    zoom: 0.90
+})
